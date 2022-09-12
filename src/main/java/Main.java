4e905fb66fe7834c418eed476846ec27458a6a32
MeH2607 <moha898h@stud.kea.dk>
2022-09-12 15:49:54 +0200

    Tilføjet menu uden en løkke

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,70 +4,25 @@ public class Main {
     public static void main(String[] args) {
 //for nu oprettes superhelte i main, senere vil vi lave en database der holder på data
 
+        //TODO lav en menu der opretter en superhelt eller lukker programmet. Loop kommer senere
        Scanner sc = new Scanner(System.in);
 
-       /*  System.out.println("Tilføj en superhelt: ");
-
-        System.out.println("\nCivil navn: ");
-        String name = sc.nextLine();
-
-
-        System.out.println("\nHar de et heltenavn? 1 for ja, 0 for nej");
-        int svar = sc.nextInt();
-        sc.nextLine();
-        String heroName = null;
-        if (svar == 1) {
-            System.out.println("\nTilføj heltenavn: ");
-            heroName = sc.nextLine();
-        }
-        System.out.println("\nTilføj superstyrke: ");
-        String heroPower = sc.nextLine();
-
-        System.out.println("\nEr de et menneske?");
-        boolean isHuman = true;
-        int valg;
-        do {
-            //todo insæt switch istedet
-            System.out.println("\nTast 1 for ja\nTast 2 for nej");
-            valg = sc.nextInt();
-            //
-            sc.nextLine();
-            if(valg == 1){
-                isHuman = true;
-            }
-            else if(valg == 2){
-                isHuman = false;
-            }
-            else System.out.println("Prøv igen");
-        } while (valg != 1 && valg != 2);
-
-
-        System.out.println("\nHvornår blev de lavet: ");
-        int year = sc.nextInt();
-
-        if (heroName == null) {
-            SuperHero hero = new SuperHero(name, heroPower, isHuman, year);
-            System.out.println("Du har tilføjet " + name + " som ikke har et helte navn\n Deres styrke er "
-                    + heroPower);
-            if (isHuman = true) {
-                System.out.println("\nDe er et menneske\nDe blev lavet i år " + year);
-            } else {
-                System.out.println("\nDe er ikke et menneske\nDe blev lavet i år " + year);
-            }
-        } else {
-            SuperHero hero = new SuperHero(name, heroName, heroPower, isHuman, year);
-            System.out.println("Du har tilføjet " + name + " også kendt som " + heroName + "\nDeres styrke er "
-                    + heroPower);
-            if (isHuman = true) {
-                System.out.println("\nDe er et menneske\nDe blev lavet i år " + year);
-            } else {
-                System.out.println("\nDe er ikke et menneske\nDe blev lavet i år " + year);
-            }
-        }*/
-
         Database database = new Database();
-        database.createAndAddHero();
 
+        int menuValg;
+
+        System.out.println("Velkommen til Superhelte databasen\n\n1. Opret superhelt\n9. afslut");
+        menuValg = sc.nextInt();
+        switch (menuValg){
+            case 1:
+                database.createAndAddHero();
+                break;
+            case 9:
+                System.out.println("Op gensyn");
+                break;
+            default:
+                System.out.println("Ugyldigt valg");
+        }
 
     }
 }
