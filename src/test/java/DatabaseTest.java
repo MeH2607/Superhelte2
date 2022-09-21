cbcef903fde8846e1ac1df9883ebc27b3dcc5379
MeH2607 <moha898h@stud.kea.dk>
2022-09-21 10:43:22 +0200

    Omkodet test så der er en beforeEach der tilføjer 4 helte til databasen. Omkodet addToDatabase, nu indsætter den 2 helte, en med og en uden superheltenavn, og tester om de begge er i listen ved at finde deres navn og se listens størrelse.

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.crypto.Data;
@@ -8,29 +9,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
 
+
     Database db = new Database();
+    SuperHero defaultHero;
+    @BeforeEach
+    void setUp(){
+        //Har egentlig bare denne her for at prøve BeforeEach af
+        SuperHero testHero1 = new SuperHero("Peter Parker", "Spider-man",
+                "Spider stuff", true, 1962);
+        SuperHero testHero2 = new SuperHero("Clark Kent", "Superman",
+                "Super strength, flight, lasers, shit what doesn't he have", false, 1938);
+        SuperHero testHero3 = new SuperHero("Johnny",
+                "Cool as shit", true, 2000);
+        SuperHero testHero4 = new SuperHero("Bruce Wayne", "Batman",
+                "Money", true, 1939);
+        db.getHeroDatabase().addAll(Arrays.asList(testHero1, testHero2, testHero3, testHero4));
+
+    }
 
 
     @Test
     void addToDatabase() {
-        SuperHero testHero = new SuperHero("Peter Parker", "Spider-man",
-                "Spider stuff", true, 1960);
-        db.getHeroDatabase().add(testHero);
-        int expectedListSize = 1;
-        String expectedName = "Peter Parker";
+        //Hvis der er mellemrum i navnet "Luke Cage " så virker equals ikke. Hvordan kan man tage højde for det?
+        db.addToDatabase("Luke Cage" ,"Super strength, ", true, 1972);
+        db.addToDatabase("John Stewart", "Green Lantern", "Green lantern ring", true, 1971);
+        int expectedListSize = 6;
+        int expectedIndexForLukeCage = 4;
+        int expectedIndexForGreenLantern = 5;
+        String expectedNameGreenLantern = "Green Lantern";
+        String expectedNameLukeCage = "Luke Cage";
 
       /*  assertEquals(expectedListSize, db.getHeroDatabase().size());
         assertEquals(expectedName, testHero.getName());*/
 
         assertAll("Tester nyindsatte helt",
                 () -> assertEquals(expectedListSize, db.getHeroDatabase().size()),
-                () -> assertEquals(expectedName, testHero.getName()),
-                () -> assertTrue(testHero.isHuman())
+                () -> assertEquals(expectedNameLukeCage, db.getHeroDatabase().get(expectedIndexForLukeCage).getName()),
+                () -> assertEquals(expectedNameGreenLantern, db.getHeroDatabase().get(expectedIndexForGreenLantern).getSuperheroName())
         );
     }
 
 
-    @Test
+   /* @Test
     void testAddManyToDatabase() {
         SuperHero testHero1 = new SuperHero("Peter Parker", "Spider-man",
                 "Spider stuff", true, 1962);
@@ -41,11 +61,12 @@ class DatabaseTest {
         SuperHero testHero4 = new SuperHero("Bruce Wayne", "Batman",
                 "Money", true, 1939);
         db.getHeroDatabase().addAll(Arrays.asList(testHero1, testHero2, testHero3, testHero4));
-        int expectedListSize = 4;
+        db.add
+        int expectedListSize = 5;
 
         assertEquals(expectedListSize, db.getHeroDatabase().size());
 
-    }
+    }*/
 
     @Test
     void searchForHeroList() {
@@ -74,7 +95,7 @@ class DatabaseTest {
         int expectedListSizeOne = 1;
         int expectedListSizeMany = 3;
 
-        assertAll("Tester nyindsatte helt",
+        assertAll("Tester nyindsatte helte for 0, 1 og 3 helte som er fundet",
                 () -> assertEquals(expectedListSizeNone, testListSizeNone),
                 () -> assertEquals(expectedListSizeOne, testListSizeOne),
                 () -> assertEquals(expectedListSizeMany, testListSizeMany)
