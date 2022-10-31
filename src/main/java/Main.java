66a6b0e0faea38fca9089d2974a2f4c6a8e3becc
MeH2607 <moha898h@stud.kea.dk>
2022-10-31 11:53:15 +0100

    Refaktoreret koden så den arbejder med controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+
+    //Controller skal styre programmet rundt, essentielt en dirrigent.
+    // Den skal ikke forstå noget uden for programmets endepunkter,
+    // så her laver vi en filehandler når der skal skrives til tekstfiler.
+
     public static void main(String[] args) {
     UserInterface ui = new UserInterface();
 
